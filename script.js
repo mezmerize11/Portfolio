d4bb282c16741432b39573dc9cd2d25d98702347
Mr Freaks <leezzyixxose@gmail.com>
2026-08-29 07:36:06 +0800

    Second Update

--- script.js
+++ script.js
@@ -10,3 +10,74 @@ document.querySelectorAll(".nav-links a").forEach(link => {
 });
 
 document.getElementById("year").textContent = new Date().getFullYear();
+
+const projectCards = document.querySelectorAll(".project-card");
+
+projectCards.forEach((card) => {
+    card.addEventListener("mouseenter", () => {
+        card.style.transition = "0.3s ease";
+    });
+});
+
+/* =========================
+   TYPING EFFECT
+========================= */
+
+const typingText = document.getElementById("typing-text");
+
+const words = [
+    "Technical Support",
+    "Customer Experience",
+    "Training",
+    "Problem Solver"
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+function typeEffect() {
+
+    const currentWord = words[wordIndex];
+
+    if (!deleting) {
+
+        typingText.textContent =
+            currentWord.substring(0, charIndex + 1);
+
+        charIndex++;
+
+        if (charIndex === currentWord.length) {
+
+            deleting = true;
+
+            setTimeout(typeEffect, 1500);
+            return;
+        }
+
+    } else {
+
+        typingText.textContent =
+            currentWord.substring(0, charIndex - 1);
+
+        charIndex--;
+
+        if (charIndex === 0) {
+
+            deleting = false;
+
+            wordIndex++;
+
+            if (wordIndex >= words.length) {
+                wordIndex = 0;
+            }
+        }
+    }
+
+    setTimeout(
+        typeEffect,
+        deleting ? 50 : 100
+    );
+}
+
+typeEffect();
